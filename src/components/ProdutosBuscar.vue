00c30c70199f3bc3f1ff72d4cc7e60c126fd4c22
Victor Oliveira <victorfelipe1608@gmail.com>
2019-06-14 00:11:19 -0300

    Implementação da função de busca

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,10 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <input name="busca" id="busca" type="text" v-model="busca">
+    <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos">
+  </div>
 </template>
 
 <script>
 export default {
-  name: "ProdutosBuscar"
+  name: "ProdutosBuscar",
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } });
+    }
+  }
 };
 </script>
 
