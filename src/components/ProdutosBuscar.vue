2665e13b48bfa83c4b13d7b78dea2b70d05ece42
Victor Oliveira <victorfelipe1608@gmail.com>
2019-06-14 23:47:49 -0300

    Estilizando Lista e Busca de Produtos

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <input name="busca" id="busca" type="text" v-model="busca">
+  <form>
+    <input name="busca" id="busca" type="text" placeholder="Buscar produtos..." v-model="busca">
     <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos">
-  </div>
+  </form>
 </template>
 
 <script>
@@ -17,4 +17,33 @@ export default {
 </script>
 
 <style>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#busca:focus,
+#busca:hover {
+  transform: scale(1.1);
+}
+
+#lupa {
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 0;
+  box-shadow: none;
+}
 </style>
